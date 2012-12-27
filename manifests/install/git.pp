a7efe26eef8aa0446775f25aa03bd557ac3e7494
Jason Edgecombe <jason@rampaginggeek.com>
2012-12-26 21:50:05 -0500

    Make the buildbot installation check out from a git repo
    Add the vcsrepo as a dependency

---
+++ manifests/install/git.pp
@@ -0,0 +1,25 @@
+# === Authors
+#
+# Jason Edgecombe <jason@rampaginggeek.com>
+#
+# === Copyright
+#
+# Copyright 2012 Jason Edgecombe, unless otherwise noted.
+#
+#
+class buildbot::install::git( $directory='/home/buildmaster/buildbot-src',
+      $owner='buildmaster',
+      $source='git://github.com/buildbot/buildbot.git',
+      $revision='master' ) {
+  include buildbot::base
+  include vcsrepo
+
+  vcsrepo { $directory:
+    ensure   => present,
+    provider => git,
+    source   => $source,
+    revision => $revision,
+    owner    => $owner,
+  }
+
+}
---
+++ manifests/params.pp
@@ -0,0 +1,11 @@
+# === Authors
+#
+# Jason Edgecombe <jason@rampaginggeek.com>
+#
+# === Copyright
+#
+# Copyright 2012 Jason Edgecombe, unless otherwise noted.
+#
+class buildbot::params {
+  
+}
