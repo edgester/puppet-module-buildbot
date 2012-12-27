251bb709452f028e63cd30fdb072f5b86c309732
Jason Edgecombe <jason@rampaginggeek.com>
2012-12-27 18:38:55 -0500

    Set up the master instance

--- manifests/master.pp
+++ manifests/master.pp
@@ -6,12 +6,26 @@
 #
 # Copyright 2012 Jason Edgecombe, unless otherwise noted.
 #
-class buildbot::master {
+class buildbot::master( $user="buildmaster", $group="buildbot", $project_dir ) {
   include buildbot::base
 
-  buildbot::user_homedir { "buildmaster":
-    group    => "buildbot",
+  buildbot::user_homedir { $user:
+    group    => $group,
     fullname => "buildbot master",
     ingroups => [],
+  }
+
+  file { $project_dir :
+    ensure => directory,
+    owner  => $user,
+    group  => $group,
+  }
+  
+  exec { "buildbot create-master $project_dir":
+    path    => ['/usr/bin','/bin'],
+    creates => "$project_dir/buildbot.tac",
+    user    => $user,
+    group   => $group,
+    require => [ Class['buildbot::install::git'], File[$project_dir] ],
   }
 }
