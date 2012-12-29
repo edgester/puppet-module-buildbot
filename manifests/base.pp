6c385aaf86b88db196ebf43d386b2362f340ab2e
Jason Edgecombe <jason@rampaginggeek.com>
2012-12-28 21:33:27 -0500

    Install sqlalchemy-migrate via pip

--- manifests/base.pp
+++ manifests/base.pp
@@ -15,6 +15,13 @@ class buildbot::base {
     name   => $buildbot::params::package_deps,
   }
 
+  package { 'python_sqlalchemy_migrate' :
+    name     => 'sqlalchemy-migrate==0.7.1',
+    ensure   => present,
+    provider => pip,
+    require  => Package['buildbot'],
+  }
+
   buildbot::user_homedir { "buildbot":
     group    => "buildbot",
     fullname => "buildbot shared user",
